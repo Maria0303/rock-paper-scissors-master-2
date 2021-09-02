225644fd361144f20a37d444f5c4cb2c53bb1db8
Maria0303 <md2294481@gmail.com>
2021-09-01 19:17:21 -0500

    esta rama tiene la pprimera parte/hoja del proyecto

---
+++ index.js
@@ -0,0 +1,19 @@
+function ocultarConEquis() {
+    document.getElementById("containerRules").style.display = "none";
+}
+
+function mostrar() {
+    document.getElementById("containerRules").style.display = "block";
+}
+
+function ocultarCirculo() {
+    document.getElementById("circulos").style.display = "none";
+}
+
+function ocultarTijeras() {
+    document.getElementById("circulos").style.display = "none";
+}
+
+function ocultarPapel() {
+    document.getElementById("circulos").style.display = "none";
+}
